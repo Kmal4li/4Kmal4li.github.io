d7828177a932b46b4e10493fabcca5a8bd9eb043
Kmal4li <m4kmalali@gmail.com>
2025-06-04 08:27:40 +0700

    adding instagram icon fix

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import profilePhoto from '../assets/images/profile.jpg';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import '../assets/styles/Main.scss';
 
 function Main() {
@@ -16,7 +17,7 @@ function Main() {
           <div className="social_icons">
             <a href="https://github.com/Kmal4li" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/muhammad-akmal-ali-3388b62a2/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-            <a href="https://www.instagram.com/m._akmal_a/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"/></a>
+            <a href="https://www.instagram.com/m._akmal_a/" target="_blank" rel="noreferrer"><InstagramIcon/></a>
           </div>
           <h1>Muhammad Akmal Ali</h1>
           <p>Junior Software Developer</p>
