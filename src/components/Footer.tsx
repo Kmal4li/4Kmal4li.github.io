9aaa94c9b5018abe88c2ef18b7d344f69327ce93
Kmal4li <m4kmalali@gmail.com>
2025-06-04 08:35:34 +0700

    fixing icon and color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import '../assets/styles/Footer.scss'
 
 function Footer() {
@@ -9,6 +10,7 @@ function Footer() {
       <div>
         <a href="https://github.com/Kmal4li" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/muhammad-akmal-ali-3388b62a2/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://www.instagram.com/m._akmal_a/" target="_blank" rel="noreferrer"><InstagramIcon/></a>
       </div>
       <p>A portfolio designed & built by <a href="https://github.com/Kmal4li" target="_blank" rel="noreferrer">Muhammad Akmal Ali</a> made with love.</p>
     </footer>
