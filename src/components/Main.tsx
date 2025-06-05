f782c14dbdcaddd16f2316119c7469ed0b5f2530
Kmal4li <m4kmalali@gmail.com>
2025-06-05 10:52:33 +0700

    Save progress before pull

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import profilePhoto from '../assets/images/profile.jpg';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -22,10 +22,12 @@ function Main() {
           <h1>Muhammad Akmal Ali</h1>
           <p>Junior Software Developer</p>
 
+          <a href="/cvakmal.pdf" download className="btn">Download My Resume</a>
+
           <div className="mobile_social_icons">
-            <a href="https://github.com/Kmal4li" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/muhammad-akmal-ali-3388b62a2/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-            <a href="https://www.instagram.com/m._akmal_a/" target="_blank" rel="noreferrer"><InstagramIcon/></a>
+            <a href="https://github.com/Kmal4li" target="_blank" rel="noreferrer"><GitHubIcon /></a>
+            <a href="https://www.linkedin.com/in/muhammad-akmal-ali-3388b62a2/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+            <a href="https://www.instagram.com/m._akmal_a/" target="_blank" rel="noreferrer"><InstagramIcon /></a>
           </div>
         </div>
       </div>
